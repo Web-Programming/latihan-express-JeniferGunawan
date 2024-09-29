d57da29619d4fdb42fb232321f2e06b911e2bf66
Michael <jenifergunawan5@gmail.com>
2024-09-29 21:11:12 +0700

    Tugas membuat contact us
    Tugas PAW II Pertemuan ke-6. Tanggal 27 Sept 2024

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,12 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); // Middleware untuk parsing urlencoded
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routing
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -36,6 +39,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Menjalankan server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+// Rute untuk halaman utama
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Rute untuk halaman about
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Us' });
+});
+
+// Rute untuk halaman contact
+router.get('/contact', function(req, res, next) { // Perbaiki kesalahan sintaksis
+  res.render('contact'); // Pastikan file contact.ejs ada di folder views
+});
+
+// Rute untuk menangani pengiriman formulir contact
+router.post('/submit-contact', function(req, res, next) {
+  const name = req.body.name;
+  res.render('submit-contact', { name: name }); // Pastikan file submit-contact.ejs ada di folder views
+});
+
+// Rute tambahan untuk send-message jika diperlukan
+router.post('/send-message', (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+  res.send('Your message has been sent successfully!');
 });
 
 module.exports = router;
