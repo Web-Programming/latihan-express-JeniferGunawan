75fd63db157d2c379aac803214e1973c1e0a39b4
Michael <jenifergunawan5@gmail.com>
2024-10-11 18:30:36 +0700

    Pertemuan ke 10 - Modifying Express for MVC

---
+++ my-express-mvc/app_server/controllers/main.js
@@ -0,0 +1,15 @@
+//controller index
+const index = (req, res) => {
+    res.render('index', {title : 'Express MVC' });
+}
+//controller kontak
+const kontak = (req, res) =>{
+    res.render('kontak', {title : 'Ini Halaman Kontak' });
+}
+
+//controller profil
+const profil = (req, res) =>{
+    res.render('profil', {title : 'Ini Halaman Profil gais' });
+}
+
+module.exports = { index, kontak, profil };
