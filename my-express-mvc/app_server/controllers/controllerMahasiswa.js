7caa9ded4e902294ae60c4aff1d52626cf4669d6
Michael <jenifergunawan5@gmail.com>
2024-10-23 20:36:40 +0700

    insert database

--- my-express-mvc/app_server/controllers/controllerMahasiswa.js
+++ my-express-mvc/app_server/controllers/controllerMahasiswa.js
@@ -1,19 +1,71 @@
 const Mahasiswa = require("../models/mahasiswa");
 
-const Index = async (req, res) => {
-    try {
-        // Mencari data mahasiswa
-        const mahasiswas = await Mahasiswa.find({});
-         // Mengirimkan data mahasiswa jika ditemukan
-        res.status(200).json(mahasiswas);
-        // Jika tidak ada data mahasiswa ditemukan
-        if (!mahasiswas) {
-            res.status(400).json({ message: "Collection is empty" });
-        }
-    } catch (error) {
-        // Mengirimkan error jika ada masalah pada server
-        res.status(500).json({ message: "Error retrieving users", error });
-    }
-}
+// const Index = async (req, res) => {
+//     try {
+//         // Mencari data mahasiswa
+//         const mahasiswas = await Mahasiswa.find({});
+//          // Mengirimkan data mahasiswa jika ditemukan
+//         res.status(200).json(mahasiswas);
+//         // Jika tidak ada data mahasiswa ditemukan
+//         if (!mahasiswas) {
+//             res.status(400).json({ message: "Collection is empty" });
+//         }
+//     } catch (error) {
+//         // Mengirimkan error jika ada masalah pada server
+//         res.status(500).json({ message: "Error retrieving users", error });
+//     }
+// }
 
-module.exports = {Index}
+const index = (req, res, next) => {
+    Mahasiswa.find({}, { __v: 0 })
+      .then((mhs) => {
+        const responseMessage = {
+            code: 200,
+            success: true,
+            message: "Successfull",
+            data: mhs
+        };
+        res.status(200).json(responseMessage);
+      })
+      .catch((e) => {
+        const responseMessage = {
+            code: 400,
+            success: false,
+            message: "Bad request"
+        };
+        res.status(400).json(responseMessage);
+      });
+};
+
+//untuk menghandle request insert mahasiswa
+const insert = (req, res, next) => {
+    const mhs = new Mahasiswa({
+      nama: req.body.nama,
+      npm: req.body.npm,
+      email: req.body.email,
+      tanggal_lahir: req.body.tanggal_lahir,
+      aktif: true
+    });
+  
+    mhs
+      .save()
+      .then((result) => {
+            const responseMessage = {
+                code: 200,
+                success: true,
+                message: "Successfull",
+                data: result
+            };
+            res.status(200).json(responseMessage);
+        })
+        .catch((e) => {
+            const responseMessage = {
+                code: 400,
+                success: true,
+                message: "Bad request"
+            };
+            res.status(400).json(responseMessage);
+        });
+};
+
+module.exports = {index,insert}
--- my-express-mvc/app_server/routes/mahasiswas.js
+++ my-express-mvc/app_server/routes/mahasiswas.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mahasiswaController = require("../controllers/controllerMahasiswa");
 
 // Fungsi dan Rute Index digunakan untuk memanggil semua data dalam database mongodb
-router.get("/mahasiswa", mahasiswaController.Index);
+router.get("/mahasiswa", mahasiswaController.index);
+router.post("/mahasiswa/insert", mahasiswaController.insert);
 module.exports = router;
