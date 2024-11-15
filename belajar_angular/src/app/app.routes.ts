61565279edb54658b0ec0d887e9ce230da03ebf8
Michael <jenifergunawan5@gmail.com>
2024-11-15 17:41:03 +0700

    routing part 2

--- belajar_angular/src/app/app.routes.ts
+++ belajar_angular/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 export const routes: Routes=[
     {
         path: 'home', 
         component: HomeComponent,
         title : 'Home Page'
-    }];
+    },
+    {
+       path : 'details',
+       component: DetailsComponent,
+       title: 'Details Page' 
+    },
+
+];
