a43317d4fe0c6c5bd13569c00d1eb22679515af8
Michael <jenifergunawan5@gmail.com>
2024-12-05 15:36:49 +0700

    add login dan register di angular

--- projectUasAngular/src/app/app.routes.ts
+++ projectUasAngular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+    {
+        path:'register',
+        component: RegisterComponent,
+        title: 'Register Page'
+    },
+    {
+        path:'login',
+        component: LoginComponent,
+        title: 'Login Page'
+    }
 
-export const routes: Routes = [];
+];
